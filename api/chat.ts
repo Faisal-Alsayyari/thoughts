1d7449bac4aed6b1b2371a8827fcc3a623f8b5fb
Faisal <sayyarifaisal@gmail.com>
2026-03-17 13:26:56 -0400

    Added system prompt to prevent headers and nested lists

--- api/chat.ts
+++ api/chat.ts
@@ -5,14 +5,33 @@ export const config = {
   runtime: 'edge',
 };
 
+/*
+ * req.method: HTTP request verb (GET, POST, etc.)
+ * req.body: JSON body (payload)
+ * req.params: URL params
+ * req.query: query string
+ * req.headers: headers
+ */
 export default async function handler(req: Request) {
+
+  // handle all GET requests like this
   if (req.method === 'GET') {
     return new Response('Chat API is working');
   }
 
+  // otherwise we expect an LLM response
+  
   if (req.method === 'POST') {
     try {
+      // extract the prompt and context from the JSON 
       const { prompt, context } = await req.json();
+
+      // Only append this system prompt on the root node, i.e. the context (from previous nodes)
+      // is empty.
+      
+      const system_prompt = "Do NOT attempt to use headers or any time of formatting outside of"
+      + "numbers, letters, or dashes for SINGLE ORDER lists. DO NOT try to use nested lists. This is a" 
+      + "VERY STRICT RULE. Again, NO HEADERS or any formatting";
 
       const messages = [
         ...(context || []),
@@ -21,6 +40,7 @@ export default async function handler(req: Request) {
 
       const result = await streamText({
         model: google('gemini-2.5-flash'),
+        system: system_prompt,
         messages,
       });
 
